dde868fb096a3426baa8d760d84090ae50b4debd
Nazariy <nazar260804@gmail.com>
2023-03-06 00:21:51 +0200

    added thirst regen to drinks and thirst on potion use

--- Changes/VanillaFoodChanges.cs
+++ Changes/VanillaFoodChanges.cs
@@ -11,7 +11,9 @@ public class VanillaFoodChanges : GlobalItem
     {
         base.OnConsumeItem(item, player);
         HungerPlayer modPlayer = player.GetModPlayer<HungerPlayer>();
+        ThirstPlayer thirstPlayer = player.GetModPlayer<ThirstPlayer>();
 
+        //hunger changes
         switch (item.buffType)
         {
             case BuffID.WellFed:
@@ -22,6 +24,42 @@ public class VanillaFoodChanges : GlobalItem
                 break;
             case BuffID.WellFed3:
                 modPlayer.Hunger += 15f;
+                break;
+ 
+        }
+
+        //drink changes
+        switch (item.type)
+        {
+            case ItemID.AppleJuice:
+            case ItemID.BloodyMoscato:
+            case ItemID.BottledWater:
+            case ItemID.FruitJuice:
+            case ItemID.BananaDaiquiri:
+            case ItemID.Lemonade:
+            case ItemID.PeachSangria:
+            case ItemID.PinaColada:
+            case ItemID.SmoothieofDarkness:
+            case ItemID.TropicalSmoothie:
+            case ItemID.Teacup:
+            case ItemID.MilkCarton:
+            case ItemID.Ale:
+            case ItemID.Sake:
+                thirstPlayer.Thirst += 5f;
+                break;
+            case ItemID.CoffeeCup:
+            case ItemID.CreamSoda:
+                thirstPlayer.Thirst += 10f;
+                break;
+            case ItemID.GrapeJuice:
+            case ItemID.Milkshake:
+                thirstPlayer.Thirst += 15f;
+                break;
+            default:
+                if (item.buffType != 0)
+                {
+                    thirstPlayer.potionThirstCooldown = 300;
+                }
                 break;
         }
     }
--- ThirstPlayer.cs
+++ ThirstPlayer.cs
@@ -16,17 +16,25 @@ public class ThirstPlayer : ModPlayer
     }
 
     private float thirstUpdateCooldown = 1000;
+    public float potionThirstCooldown = 0;
 
     public override void PostUpdateMiscEffects()
     {
         if (Player.whoAmI == Main.myPlayer)
         {
+            if (potionThirstCooldown > 0)
+            {
+                ThirstLoss += 3;
+                potionThirstCooldown -= 1;
+            }
+            
             thirstUpdateCooldown -= ThirstLoss;
             if (thirstUpdateCooldown <= 0)
             {
                 Thirst -= 1;
                 thirstUpdateCooldown = 1000;
             }
+         
         }
     }
 }
