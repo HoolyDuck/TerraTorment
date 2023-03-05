1782ef19112dc13918afb325ba4de362c8255d96
Nazariy <nazar260804@gmail.com>
2023-03-05 02:40:08 +0200

    simplified foodchange system

--- Content/Hunger/HungerChanges/ActionChanges/VanillaFoodChanges.cs
+++ Content/Hunger/HungerChanges/ActionChanges/VanillaFoodChanges.cs
@@ -10,11 +10,41 @@ public class VanillaFoodChanges : GlobalItem
     public override void OnConsumeItem(Item item, Player player)
     {
         base.OnConsumeItem(item, player);
-        foreach (FoodChange foodChange in HungerSystem.FoodChanges) {
-            if (item.type == foodChange.FoodItemId)
-            {
-                player.GetModPlayer<HungerPlayer>().Hunger += foodChange.AddHunger;
-            }
+        HungerPlayer modPlayer = player.GetModPlayer<HungerPlayer>();
+
+        switch (item.type)
+        {
+            case ItemID.Apple:
+            case ItemID.Banana:
+            case ItemID.Grapes:
+            case ItemID.Lemon:
+            case ItemID.Apricot:
+            case ItemID.BlackCurrant:
+            case ItemID.BloodOrange:
+            case ItemID.Cherry:
+            case ItemID.Coconut:
+            case ItemID.Grapefruit:
+            case ItemID.Mango:
+            case ItemID.Peach:
+            case ItemID.Pineapple:
+            case ItemID.Plum:
+            case ItemID.Elderberry:
+            case ItemID.Rambutan:
+            case ItemID.ShuckedOyster:
+            case ItemID.Marshmallow:
+                modPlayer.Hunger += 5f;
+                break;
+
+            case ItemID.CookedMarshmallow:
+            case ItemID.BunnyStew:
+            case ItemID.CookedFish:
+            case ItemID.FruitSalad:
+            case ItemID.GrilledSquirrel:
+            case ItemID.RoastedBird:
+            case ItemID.SauteedFrogLegs:
+            case ItemID.PotatoChips:
+                modPlayer.Hunger += 10f;
+                break;
         }
     }
 }
