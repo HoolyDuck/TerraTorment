2b6e28b604aeaf52558e717977eb851208387649
Nazariy <nazar260804@gmail.com>
2023-03-06 02:04:57 +0200

    temporarily add env temp display here

--- UI/HungerBar.cs
+++ UI/HungerBar.cs
@@ -11,6 +11,7 @@ public class HungerBar : UIState
 {
     public UIText hungerText;
     public UIText thirstText;
+    public UIText envTemperatureText;
 
     public override void OnInitialize()
     {
@@ -27,6 +28,16 @@ public class HungerBar : UIState
         thirstText.Width.Set(200, 0f);
         thirstText.Height.Set(50, 0f);
         Append(thirstText);
+        
+        // move elsewhere later
+        envTemperatureText = new UIText("Environment Temperature: 22");
+        envTemperatureText.Top.Set(80, 0f);
+        envTemperatureText.Left.Set(0, .7f);
+        envTemperatureText.Width.Set(200, 0f);
+        envTemperatureText.Height.Set(50, 0f);
+        Append(envTemperatureText);
+
+
     }
     
     public override void Update(GameTime gameTime)
@@ -34,8 +45,10 @@ public class HungerBar : UIState
         base.Update(gameTime);
         var hungerPlayer = Main.LocalPlayer.GetModPlayer<HungerPlayer>();
         var thirstPlayer = Main.LocalPlayer.GetModPlayer<ThirstPlayer>();
+        var temperaturePlayer = Main.LocalPlayer.GetModPlayer<TemperaturePlayer>();
         hungerText.SetText($"Hunger: {hungerPlayer.Hunger}, HungerChange: {hungerPlayer.hungerChange}");
         thirstText.SetText($"Thirst: {thirstPlayer.Thirst}, ThirstLoss: {thirstPlayer.ThirstLoss}");
+        envTemperatureText.SetText($"Environment Temperature: {temperaturePlayer.environmentTemperature}");
         
     }
 }
