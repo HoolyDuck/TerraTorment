70e73b9e1c9c78bcfc7e33b840b888305c6e78db
Nazariy <nazar260804@gmail.com>
2023-03-07 18:12:37 +0200

    add first potion item change

---
+++ Changes/ItemChanges/Potions/Drinks/AppleJuice.cs
@@ -0,0 +1,22 @@
+using Terraria;
+using Terraria.ID;
+
+namespace TerraTorment.Changes.ItemChanges.Potions.Drinks;
+
+public class AppleJuice : PotionChange
+{
+    public override int ItemId => ItemID.AppleJuice;
+    
+    public override float GetThirstGain => 5f;
+
+    public override void OnConsumeItem(Item item, Player player)
+    {
+        base.OnConsumeItem(item, player);
+        AddEffects(player);
+    }
+    
+    private void AddEffects(Player player)
+    {
+        player.AddBuff(BuffID.Ironskin, 3600);
+    }
+}
